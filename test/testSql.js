76fdad62faa34d39987d2b6afe3fd05f63e06b1e
zhouxianjun <zxj19910611>
2017-03-23 22:18:28 +0800

    distribute job

--- test/testSql.js
+++ test/testSql.js
@@ -1,14 +1,11 @@
 /**
  * Created by Alone on 2017/3/1.
  */
-const Utils = require('../lib/Utils');
-const DigitsRule = require('../lib/vaildate/rule/Digits');
-let ruleMap = new Map();
-ruleMap.set(['a.list[*].val'], [new DigitsRule(1, 10)]);
-Utils.validate({
-    a: {
-        list: [{
-            val: 1
-        }]
+const DB = require('../lib/DB');
+let Job = DB.sequelize.model('Job');
+let job = Job.findOne({
+    where:  {
+        taskId: '11'
     }
-}, ruleMap);
+});
+console.log(job);
